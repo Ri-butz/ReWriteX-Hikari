8885044951627b806ae9c72f0729b634ff5e1ab9
Ri_Butz <104504950+Aozora69@users.noreply.github.com>
2022-09-19 17:16:51 +0700

    Update rewrite_balance.sh

--- system/etc/rewrite/rewrite_balance.sh
+++ system/etc/rewrite/rewrite_balance.sh
@@ -1,8 +1,22 @@
 #!/system/bin/sh
 
+SCHED_TASKS=10
+SCHED_PERIOD=$((1 * 1000 * 1000))
+
 # Enable Thermal
 su -c start thermal-engine
 su -c start vendor.thermal-engine
+
+# Kernel parameters
+echo "3" > /proc/sys/kernel/perf_cpu_time_max_percent
+echo "$SCHED_PERIOD" > /proc/sys/kernel/sched_latency_ns
+echo "$((SCHED_PERIOD / 2))" > /proc/sys/kernel/sched_wakeup_granularity_ns
+echo "$((SCHED_PERIOD / SCHED_TASKS))" > /proc/sys/kernel/sched_min_granularity_ns
+echo "5000000" > /proc/sys/kernel/sched_migration_cost_ns
+echo "4" > /proc/sys/kernel/sched_nr_migrate
+echo "1" > /proc/sys/kernel/sched_autogroup_enabled
+echo "0" > /proc/sys/kernel/sched_tunable_scaling
+echo "1" > /proc/sys/kernel/sched_child_runs_first
 
 # Core Control
 ##cpuset
@@ -44,9 +58,9 @@ chmod 444 /sys/devices/system/cpu/cpu7/online
 for gov in /sys/devices/system/cpu/*/cpufreq
 do
     echo "schedutil" > $gov/scaling_governor
-    echo "1000" > $gov/schedutil/up_rate_limit_us
-    echo "20000" > $gov/schedutil/down_rate_limit_us
-    echo "80" > $gov/schedutil/hispeed_load
+    echo "0" > $gov/schedutil/up_rate_limit_us
+    echo "0" > $gov/schedutil/down_rate_limit_us
+    echo "85" > $gov/schedutil/hispeed_load
     echo "0" > $gov/schedutil/pl
 done
 
