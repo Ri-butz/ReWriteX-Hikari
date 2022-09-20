50172b7c93411eb9a7dc94b0ce3aab02895f4f2b
Ri_Butz <104504950+Aozora69@users.noreply.github.com>
2022-09-21 06:17:35 +0700

    Update rewrite_balance.sh

--- system/etc/rewrite/rewrite_balance.sh
+++ system/etc/rewrite/rewrite_balance.sh
@@ -1,22 +1,8 @@
 #!/system/bin/sh
 
-SCHED_TASKS=10
-SCHED_PERIOD=$((1 * 1000 * 1000))
-
 # Enable Thermal
 su -c start thermal-engine
 su -c start vendor.thermal-engine
-
-# Kernel parameters
-echo "3" > /proc/sys/kernel/perf_cpu_time_max_percent
-echo "$SCHED_PERIOD" > /proc/sys/kernel/sched_latency_ns
-echo "$((SCHED_PERIOD / 2))" > /proc/sys/kernel/sched_wakeup_granularity_ns
-echo "$((SCHED_PERIOD / SCHED_TASKS))" > /proc/sys/kernel/sched_min_granularity_ns
-echo "5000000" > /proc/sys/kernel/sched_migration_cost_ns
-echo "4" > /proc/sys/kernel/sched_nr_migrate
-echo "1" > /proc/sys/kernel/sched_autogroup_enabled
-echo "0" > /proc/sys/kernel/sched_tunable_scaling
-echo "1" > /proc/sys/kernel/sched_child_runs_first
 
 # Core Control
 ##cpuset
@@ -78,6 +64,7 @@ echo "0" > /sys/class/kgsl/kgsl-3d0/force_no_nap
 echo "0" > /sys/class/kgsl/kgsl-3d0/force_rail_on
 echo "0" > /sys/class/kgsl/kgsl-3d0/force_bus_on
 echo "0" > /sys/class/kgsl/kgsl-3d0/force_clk_on
+echo "500" > /sys/class/kgsl/kgsl-3d0/pmqos_active_latency
 
 # Entropy
 echo "128" > /proc/sys/kernel/random/read_wakeup_threshold
