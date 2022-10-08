ca9c0a61d779bbf81fa99b78be717bc75d5b55aa
Ri_Butz <104504950+Aozora69@users.noreply.github.com>
2022-10-08 14:33:14 +0700

    Update service.sh

--- service.sh
+++ service.sh
@@ -8,25 +8,69 @@ MODDIR=${0%/*}
 
 # Sleep some time to make sure init is completed
 sleep 30
-wait_until_login() {
-  # In case of /data encryption is disabled
-  while [[ "$(getprop sys.boot_completed)" != "1" ]]; do
-    sleep 3
-  done
-  # We don't have the permission to rw "/storage/emulated/0" before the user unlocks the screen
-  test_file="/storage/emulated/0/Android/.PERMISSION_TEST"
-  true >"$test_file"
-  while [[ ! -f "$test_file" ]]; do
-    true >"$test_file"
-    sleep 1
-  done
-  rm -f "$test_file"
-}
-wait_until_login
-
 # Enable all tweak
 sed -Ei 's/^description=(\[.*][[:space:]]*)?/description=[ 🚴 Apply tweaks please wait... ] /g' "/data/adb/modules/ReWrite/module.prop"
 su -lp 2000 -c "cmd notification post -S bigtext -t 'Re-WriteX' tag '🚴 Apply tweaks please wait...'" >/dev/null 2>&1
+
+# Basic tool function
+ps_ret=""
+write() {
+	# Bail out if file does not exist
+	[[ ! -f "$1" ]] && return 1
+
+	# Make file write-able in case it is not already
+	chmod +w "$1" 2>/dev/null
+
+	# Write the new value and bail if there's an error
+	if ! echo "$2" > "$1" 2>/dev/null
+	then
+		echo "[️X] Failed: $1 --> $2"
+		return 1
+	fi
+
+	# Log the success
+	echo "[✓] Success: $1 --> $2"
+}
+
+# Cgroup
+change_task_cgroup() {
+    local ps_ret
+    ps_ret="$(ps -Ao pid,args)"
+    for temp_pid in $(echo "$ps_ret" | grep "$1" | awk '{print $1}'); do
+        for temp_tid in $(ls "/proc/$temp_pid/task/"); do
+            echo "$temp_tid" > "/dev/$3/$2/tasks"
+        done
+    done
+}
+
+# Clear top-app
+for clear in $(cat /dev/cpuset/top-app/tasks); do
+    echo "$clear" >/dev/cpuset/foreground/tasks
+done
+
+# Reduce render thread waiting time
+change_task_cgroup "surfaceflinger" "" "cpuset"
+change_task_cgroup "surfaceflinger" "top-app" "cpuset"
+change_task_cgroup "surfaceflinger" "foreground" "stune"
+
+# Better rendering speed
+change_task_cgroup "android.hardware.graphics.composer" "top-app" "cpuset"
+change_task_cgroup "android.hardware.graphics.composer" "foreground" "stune"
+
+# Renicer
+function high_priority() {
+  pgrep -f $1 | while read pid; do
+  renice -n +40 -p $pid
+  renice -n -19 -p $pid
+  renice -n -15 -p $pid
+  echo "$pid" > /dev/cpuset/top-app/cgroup.procs
+  echo "$pid" > /dev/stune/top-app/cgroup.procs
+  done
+}
+
+# Set high priority
+high_priority android.hardware.graphics.composer
+high_priority surfaceflinger
 
 # AVC denial fix
 magiskpolicy --live 'allow untrusted_app proc_net_tcp_udp file {read write open getattr}'
@@ -39,27 +83,29 @@ iptables -t nat -I OUTPUT -p tcp --dport 53 -j DNAT --to-destination :53
 iptables -t nat -I OUTPUT -p udp --dport 53 -j DNAT --to-destination :53
 
 # Kernel parameters
-SCHED_TASKS=10
-SCHED_PERIOD=$((1 * 1000 * 1000))
-echo "3" > /proc/sys/kernel/perf_cpu_time_max_percent
-echo "$SCHED_PERIOD" > /proc/sys/kernel/sched_latency_ns
-echo "$((SCHED_PERIOD / 2))" > /proc/sys/kernel/sched_wakeup_granularity_ns
-echo "$((SCHED_PERIOD / SCHED_TASKS))" > /proc/sys/kernel/sched_min_granularity_ns
+echo "0" > /proc/sys/kernel/sched_boost
+echo "15" > /proc/sys/kernel/perf_cpu_time_max_percent
+echo "4000000" > /proc/sys/kernel/sched_latency_ns
+echo "15000000" > /proc/sys/kernel/sched_wakeup_granularity_ns
+echo "10000000" > /proc/sys/kernel/sched_min_granularity_ns
 echo "5000000" > /proc/sys/kernel/sched_migration_cost_ns
-echo "4" > /proc/sys/kernel/sched_nr_migrate
-echo "1" > /proc/sys/kernel/sched_autogroup_enabled
+echo "32" > /proc/sys/kernel/sched_nr_migrate
+echo "95 85" > /proc/sys/kernel/sched_upmigrate
+echo "95 60" > /proc/sys/kernel/sched_downmigrate
+echo "15" > /proc/sys/kernel/sched_min_task_util_for_boost
+echo "1000" > /proc/sys/kernel/sched_min_task_util_for_colocation
+echo "100" > /proc/sys/kernel/sched_rr_timeslice_ns
+echo "1000000" > /proc/sys/kernel/sched_rt_period_us
+echo "950000" > /proc/sys/kernel/sched_rt_runtime_us
+echo "0" > /proc/sys/kernel/sched_autogroup_enabled
 echo "0" > /proc/sys/kernel/sched_tunable_scaling
-echo "1" > /proc/sys/kernel/sched_child_runs_first
+echo "0" > /proc/sys/kernel/sched_child_runs_first
+echo "0" > /proc/sys/kernel/timer_migration
+echo "0" > /proc/sys/kernel/sched_schedstats
 
 # Lpm
 echo "0" > /sys/module/lpm_levels/parameters/lpm_prediction
 echo "0" > /sys/module/lpm_levels/parameters/sleep_disabled
-
-# Disable Stats
-echo "0" > /proc/sys/kernel/sched_schedstats
-
-# Disable timer migration
-echo "0" > /proc/sys/kernel/timer_migration
 
 # Disable kernel panic
 echo "0" > /proc/sys/kernel/panic
@@ -76,6 +122,9 @@ echo "N" > /sys/module/sync/parameters/fsync_enable
 
 # Disable CRC
 echo "0" > /sys/module/mmc_core/parameters/use_spi_crc
+
+# Cpu Efficient
+echo "Y" > /sys/module/workqueue/parameters/power_efficient
 
 # Disable cpu input boost
 echo "0" > /sys/module/cpu_boost/parameters/sched_boost_on_input
@@ -104,8 +153,8 @@ swapon /dev/block/zram0 > /dev/null 2>&1
 stop perfd
 echo "10" > /proc/sys/vm/dirty_background_ratio
 echo "30" > /proc/sys/vm/dirty_ratio
-echo "3000" > /proc/sys/vm/dirty_expire_centisecs
-echo "3000" > /proc/sys/vm/dirty_writeback_centisecs
+echo "300" > /proc/sys/vm/dirty_expire_centisecs
+echo "700" > /proc/sys/vm/dirty_writeback_centisecs
 echo "750" > /proc/sys/vm/extfrag_threshold
 echo "100" > /proc/sys/vm/swappiness
 echo "0" > /proc/sys/vm/page-cluster
@@ -149,32 +198,22 @@ done
 # open source loving GL-DP and all contributors;
 # Patches Google Play services app and its background
 # processes to be able using battery optimization
-(   
-    # Wait until boot completed
-    until [ $(resetprop sys.boot_completed) -eq 1 ] &&
-        [ -d /sdcard ]; do
-        sleep 60
+# GMS components
+GMS="com.google.android.gms"
+GC1="auth.managed.admin.DeviceAdminReceiver"
+GC2="mdm.receivers.MdmDeviceAdminReceiver"
+GC3="chimera.GmsIntentOperationService"
+NLL="/dev/null"
+
+# Disable collective device administrators
+for U in $(ls /data/user); do
+    for C in $GC1 $GC2 $GC3; do
+        pm disable --user $U "$GMS/$GMS.$C" &> $NLL
     done
+done
 
-    # GMS components
-    GMS="com.google.android.gms"
-    GC1="auth.managed.admin.DeviceAdminReceiver"
-    GC2="mdm.receivers.MdmDeviceAdminReceiver"
-    GC3="chimera.GmsIntentOperationService"
-    NLL="/dev/null"
-
-    # Disable collective device administrators
-    for U in $(ls /data/user); do
-        for C in $GC1 $GC2 $GC3; do
-            pm disable --user $U "$GMS/$GMS.$C" &> $NLL
-        done
-    done
-
-    # Add GMS to battery optimization
-    dumpsys deviceidle whitelist -com.google.android.gms &> $NLL
-
-    exit 0
-)
+# Add GMS to battery optimization
+dumpsys deviceidle whitelist -com.google.android.gms &> $NLL
 
 # Fstrim
 su -c "fstrim -v /data"
