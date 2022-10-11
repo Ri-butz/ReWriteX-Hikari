c38c294248f5019e25a0dbe17e168bc456216c4b
Ri_Butz <104504950+Aozora69@users.noreply.github.com>
2022-10-11 19:56:13 +0700

    Update service.sh

--- service.sh
+++ service.sh
@@ -134,26 +134,8 @@ do
     echo "100000" > "$queue/iosched/target_latency_us"
 done
 
-# Universal GMS Doze by the
-# open source loving GL-DP and all contributors;
-# Patches Google Play services app and its background
-# processes to be able using battery optimization
-# GMS components
-GMS="com.google.android.gms"
-GC1="auth.managed.admin.DeviceAdminReceiver"
-GC2="mdm.receivers.MdmDeviceAdminReceiver"
-GC3="chimera.GmsIntentOperationService"
-NLL="/dev/null"
-
-# Disable collective device administrators
-for U in $(ls /data/user); do
-    for C in $GC1 $GC2 $GC3; do
-        pm disable --user $U "$GMS/$GMS.$C" &> $NLL
-    done
-done
-
 # Add GMS to battery optimization
-dumpsys deviceidle whitelist -com.google.android.gms &> $NLL
+dumpsys deviceidle whitelist -com.google.android.gms
 
 # Fstrim
 su -c "fstrim -v /data"
