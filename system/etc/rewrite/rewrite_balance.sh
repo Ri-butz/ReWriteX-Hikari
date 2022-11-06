fb8e7da782fd6ecb6786821d9d98bc17f73eb4f5
Ri_Butz <104504950+Ri-butz@users.noreply.github.com>
2022-11-07 06:41:19 +0700

    Update rewrite_balance.sh

--- system/etc/rewrite/rewrite_balance.sh
+++ system/etc/rewrite/rewrite_balance.sh
@@ -66,7 +66,6 @@ echo "0" > /dev/stune/top-app/schedtune.sched_boost_no_override
 
 # Gpu
 MIN=$(cat /sys/class/kgsl/kgsl-3d0/min_pwrlevel)
-echo "414000000" > /sys/class/kgsl/kgsl-3d0/max_gpuclk
 echo "1000" > /sys/class/kgsl/kgsl-3d0/idle_timer
 echo "$MIN" > /sys/class/kgsl/kgsl-3d0/default_pwrlevel
 echo "1" > /sys/class/kgsl/kgsl-3d0/throttling
