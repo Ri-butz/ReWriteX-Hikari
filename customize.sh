bbe881e65eb8400a05426f18ef1da11a6b80883f
Ri_Butz <104504950+Ri-butz@users.noreply.github.com>
2022-12-09 22:00:46 +0700

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -25,9 +25,9 @@ ui_print " Module info: "
 sleep 0.5
 ui_print " • Name            : ReWriteX-Hikari"
 sleep 0.5
-ui_print " • Version         : v7.9 "
+ui_print " • Version         : v7.9 Rev2 "
 sleep 0.5
-ui_print " • Release Date    : 02-12-2022"
+ui_print " • Release Date    : 09-12-2022"
 sleep 0.5
 ui_print " • Owner           : Ri_Butz "
 sleep 0.5
