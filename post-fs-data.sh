2b817495897931ee2a2463ad70141a2cc64b9133
Ri_Butz <104504950+Ri-butz@users.noreply.github.com>
2023-01-06 12:27:44 +0700

    Update post-fs-data.sh

--- post-fs-data.sh
+++ post-fs-data.sh
@@ -7,23 +7,3 @@ for i in $conflict
 do
 sed -i '/allow-in-power-save package="com.google.android.gms"/d;/allow-in-data-usage-save package="com.google.android.gms"/d' $i
 done
-
-# DNS Changer
-resetprop -n net.dns1
-resetprop -n net.dns2
-resetprop -n net.eth0.dns1
-resetprop -n net.eth0.dns2
-resetprop -n net.ppp0.dns1
-resetprop -n net.ppp0.dns2
-resetprop -n net.rmnet0.dns1
-resetprop -n net.rmnet0.dns2
-resetprop -n net.rmnet1.dns1
-resetprop -n net.rmnet1.dns2
-resetprop -n net.pdpbr1.dns1
-resetprop -n net.pdpbr1.dns2
-resetprop -n net.lte.dns1
-resetprop -n net.lte.dns2
-iptables -t nat -A OUTPUT -p tcp --dport 53 -j DNAT --to-destination :53
-iptables -t nat -A OUTPUT -p udp --dport 53 -j DNAT --to-destination :53
-iptables -t nat -I OUTPUT -p tcp --dport 53 -j DNAT --to-destination :53
-iptables -t nat -I OUTPUT -p udp --dport 53 -j DNAT --to-destination :53
